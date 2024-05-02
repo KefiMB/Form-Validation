7d222328b1791557713595191c18929e2877105f
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-01 22:25:33 -0400

    Part2

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ errorDisplay.textContent = "errorMessage";
 errorDisplinnerHtml = (<p>Trying to figure this out</p>);
 console.log(errorDisplay)
 
-.
+// Part 2
+form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+const input = form.querySelectorAll ("input, textarea, select");
+inputs.forEach(input => {
+    input.addEventListener('input', handleInputChange);
+  });
+function handleSubmit(event) {
+    event.preventDefault();
+
     let isValid = true;
     let inValdi = false;
 }
