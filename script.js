a1345afcd084e5f4d1e97437d50e308176591e71
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-01 22:24:42 -0400

    Part1

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Form Validation")
+
+//Part 1
+const form = document.getElementById("errorDisplay");
+errorDisplay.style.display = 'block';
+errorDisplay.textContent = "errorMessage";
+
+errorDisplinnerHtml = (<p>Trying to figure this out</p>);
+console.log(errorDisplay)
+
+.
+    let isValid = true;
+    let inValdi = false;
+}
+function showError ()
+
